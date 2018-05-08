633a314fe2c8b381d668913cc30136178f3f7683
Tom Wagner <wagne868@umn.edu>
2018-05-08 10:39:57 -0400

    Clean up syntax within ProjectView.js

--- client/src/containers/ProjectView.js
+++ client/src/containers/ProjectView.js
@@ -12,52 +12,50 @@ class ProjectView extends Component {
 		this.state = {
 		  data: {nodes:[], links:[]},
 		  selectedData: {},
-		}
-	  }
+		};
+	}
 
-	  componentDidMount() {
-			// TO GO TO DUMMY DATA:
-			axios.get('/api/data').then(result => {
-				this.setState({
-					data: result.data
-				})
-			})
+	componentDidMount() {
+		// TO GO TO DUMMY DATA:
+		// axios.get('/api/data').then(result => {
+		// 	this.setState({
+		// 		data: result.data
+		// 	});
+		// });
 
-			// TO GO TO DATABASE:
-			// axios.get('/api/projectData', { params: { id: 1 } }).then(result => {
-			// 	console.log(result);
-			// 	this.setState({data: result.data})
-			// })
-	  }
+		// TO GO TO DATABASE:
+		axios.get('/api/projectData', { params: { id: 1 } }).then(result => {
+			console.log(result);
+			this.setState({data: result.data})
+		});
+	}
 
-	  clickFunction(data) {
+	clickFunction(data) {
 		this.setState({
-		  selectedData: data
-		})
-	  }
+			selectedData: data
+		});
+	}
 
-	  createNode() {
+	createNode() {
 		const newNode = {
 			id: this.state.data.nodes.length + 1,
 			data: "this should work please"
-		}
+		};
 
 		const newData = Object.create(this.state.data);
 		newData.nodes.push(newNode);
 		this.setState({data: newData});
-	  }
-	
-	  render() {
+	}
+
+	render() {
 		return (
-		  <div className={ styles.col_12_of_12 }>
-			<h1> Project View </h1>
-			<WillowCore createNode={this.createNode.bind(this)} clickFunction={this.clickFunction.bind(this)} data={this.state.data}/>
-			<RenderedDoc docInfo={this.state.selectedData}/>
-		  </div>
+			<div className={ styles.col_12_of_12 }>
+				<h1> Project View </h1>
+				<WillowCore createNode={this.createNode.bind(this)} clickFunction={this.clickFunction.bind(this)} data={this.state.data}/>
+				<RenderedDoc docInfo={this.state.selectedData}/>
+			</div>
 		);
-	  }	
-
-
+	}
 }
 
 export default ProjectView;
