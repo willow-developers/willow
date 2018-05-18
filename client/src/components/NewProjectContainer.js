25796482d070bac3b44b04f5643441b6f1f3d719
Tom Wagner <wagne868@umn.edu>
2018-05-18 11:55:13 -0400

    Redirects flowing properly throughout

--- client/src/components/NewProjectContainer.js
+++ client/src/components/NewProjectContainer.js
@@ -1,42 +1,56 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import styles from '../assets/sass/Home.module.scss';
+import { projectGetData, resetRedirects } from '../actions/project';
+import { openModal } from '../actions/modal';
 
-// old route:
-import DisplayModal from '../containers/Modal/DisplayModal';
-
-// new route:
-// import DisplayModal from '../containers/Modal_NEW/DisplayModal';
 
 import NewProject from '../containers/NewProjectBuilder/NewProject';
-// import Modals from './Modals';
+import Modals from '../containers/Modal/Modals';
 // import Custom from './types/Custom';
 
 class NewProjectContainer extends Component {
   componentDidMount() {
-    console.log('firing!');
+    this.props.openModal({
+      id: 4,
+      type: 'custom',
+      onClose: () => console.log("fire at closing event on custom"),
+      onConfirm: () => console.log("fire at confirming event on custom"),
+      content: <NewProject />
+    });
   }
 
   render() {
-    return (
-      <div className={styles.col_12_of_12}>
-        <h1>Welcome to the Willow!</h1>
-      </div>
-    );
+    if (this.props.shouldRedirect && this.props.shouldRedirectTo) {
+      // make sure data is available as we redirect to the project
+			let projectID = this.props.shouldRedirectTo;
+			this.props.projectGetData(projectID);
+			// reset redirects in store to ensure user can navigate back to dashboard
+			this.props.resetRedirects();
+			return (
+				<Redirect to={{ pathname:`/project/${this.props.shouldRedirectTo}`}}/>
+      );
+    } else {
+      return (
+        <div className={styles.col_12_of_12}>
+          <Modals />
+        </div>
+      );
+    }
   }
 };
 
 const mapStateToProps = (state) => ({
-	userStatus: state.userStatus,
-	projectsList: state.projectsList,
-	shouldRedirect: state.shouldRedirect,
-	shouldRedirectTo: state.shouldRedirectTo,
+    isModalOpen: state.isModalOpen,
+    shouldRedirect: state.shouldRedirect,
+	  shouldRedirectTo: state.shouldRedirectTo
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	// projectsGetList: (userID) => dispatch(projectsGetList(userID)),
-	// projectGetData: (projectID) => dispatch(projectGetData(projectID)),
-	// resetRedirects: () => dispatch(resetRedirects()),
+	projectGetData: (projectID) => dispatch(projectGetData(projectID)),
+  resetRedirects: () => dispatch(resetRedirects()),
+  openModal: (modalObj) => dispatch(openModal(modalObj)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewProjectContainer);
