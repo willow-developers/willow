5bb8ca8772f2ee592e70d8267d9da6fc74b018cc
mrMustachos <bill.domanick@crowdsurge.com>
2018-05-19 14:48:27 -0400

    notes working but ugly and no markdown

--- client/src/actions/notes.js
+++ client/src/actions/notes.js
@@ -1,10 +1,13 @@
-import { ADD_NOTE, TOGGLE_PREVIEW_NOTE, NOTE_SHOW_FORM, NOTE_SHOW_LIST } from "./types";
+import { ADD_NOTE, TOGGLE_PREVIEW_NOTE, NOTE_SHOW_FORM, NOTE_SHOW_LIST, EDIT_NOTE, UPDATE_NOTE } from "./types";
 
 export const noteAdd = (data) => ({ type: ADD_NOTE, data });
 export const noteTogglePreview = (id) => ({ type: TOGGLE_PREVIEW_NOTE, id });
 
 export const noteShowForm = (bool) => ({ type: NOTE_SHOW_FORM, showForm: bool });
 export const noteShowList = (bool) => ({ type: NOTE_SHOW_LIST, showList: bool });
+
+export const noteEdit = (data) => ({ type: EDIT_NOTE, editNote: data });
+export const noteUpdate = (data) => ({ type: UPDATE_NOTE, data });
 
 
 export const addNote = (data) => (dispatch) => {
@@ -21,6 +24,17 @@ export const createNoteView = () => (dispatch) => {
 };
 
 export const closeNoteView = () => (dispatch) => {
+	dispatch(noteEdit({ id: '', title: '', content: '' }));
   dispatch(noteShowList(true));
   dispatch(noteShowForm(false));
+};
+
+export const editNote = (data) => (dispatch) => {
+	dispatch(noteEdit(data));
+  dispatch(noteShowForm(true));
+  dispatch(noteShowList(false));
+};
+
+export const updateNote = (data) => (dispatch) => {
+  dispatch(noteUpdate(data));
 };
--- client/src/containers/Notes/Card.js
+++ client/src/containers/Notes/Card.js
@@ -2,14 +2,16 @@ import React from 'react';
 import moment from 'moment';
 // import styles from '../../assets/sass/Card.module.scss';
 
-const currentTimestamp = (time) => moment(time).format('MMMM Do YYYY, h:mm: ss a');
+const currentTimestamp = (time) => moment(time).format('MMMM Do YYYY, h:mm a');
 const createPreviewText = (string) => string.split('').slice(0,125).join('').concat('...');
 
-const Card = ({ id, title, content, createdAt, togglePreview, previewToggleNote }) => (
+const Card = ({ id, title, content, createdAt, togglePreview, previewToggleNote, editNote }) => (
   <div className="card">
     <div className="header">
       <h4 className="title">{ id }: { title }</h4>
-      <button onClick={ () => console.log('load the form') }>Edit</button>
+      <button onClick={ () => {
+        editNote({ id, title, content })
+      }}>Edit</button>
     </div>
     <div className="content" onClick={ () => previewToggleNote(id) }>{ togglePreview ? content : createPreviewText(content) }</div>
     <div className="footer">
