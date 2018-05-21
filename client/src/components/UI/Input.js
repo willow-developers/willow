79837d0e3ea6a2863e04eedb82acbbdb208339f2
mrMustachos <bill.domanick@crowdsurge.com>
2018-05-21 08:16:03 -0400

    working on the styling of the notes still

--- client/src/components/UI/Input.js
+++ client/src/components/UI/Input.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classNames from 'classnames';
 import styles from '../../assets/sass/Input.module.scss';
 import Button from './Button';
 
+const DEFAULT_HEIGHT = 36;
 
 class BookmarkInput extends Component {
-	state = { isActive: false }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isActive: false,
+			height: DEFAULT_HEIGHT
+		}
+
+		this.setFilledTextareaHeight = this.setFilledTextareaHeight.bind(this);
+	}
+
+	componentDidMount() {
+		this.setFilledTextareaHeight();
+	}
+
+	setFilledTextareaHeight() {
+		const element = this.ghost;
+
+		if (element) {
+			let extra = element.clientHeight + 12;
+			this.setState({ height: extra });
+		}
+	}
 
 	render() {
 		const { input, label, type, placeholder, meta: { error, touched, valid, pristine, submitting, reset }, editMilestone } = this.props;
@@ -32,7 +54,7 @@ class BookmarkInput extends Component {
 						iconSide={ 'right' }
 						type="submit"
 						styleClass={ 'noShadow' }
-						disabledStyle={ !valid || pristine || submitting ? true : false  }
+						disabledStyle={ !valid || pristine || submitting ? true : false	}
 					/>
 				);
 			}
@@ -44,7 +66,7 @@ class BookmarkInput extends Component {
 						iconSide={ 'right' }
 						type="submit"
 						styleClass={ 'noShadow' }
-						disabledStyle={ !valid || pristine || submitting ? true : false  }
+						disabledStyle={ !valid || pristine || submitting ? true : false	}
 					/>
 				);
 			}
@@ -56,14 +78,14 @@ class BookmarkInput extends Component {
 							iconSide={ 'right' }
 							type="submit"
 							styleClass={ 'noShadow' }
-							disabledStyle={ pristine || submitting ? true : false  }
+							disabledStyle={ pristine || submitting ? true : false	}
 						/>
 						<Button
 							icon={ 'undo' }
 							iconSide={ 'center' }
 							handleClick={ reset }
 							styleClass={ 'skinnyIcon' }
-							disabledStyle={ pristine || submitting ? true : false  }
+							disabledStyle={ pristine || submitting ? true : false	}
 						/>
 						<Button
 							icon={ 'cancel' }
@@ -77,9 +99,44 @@ class BookmarkInput extends Component {
 			}
 			return;
 		}
+		
+		const getExpandableField = () => {
+			const isOneLine = this.state.height <= DEFAULT_HEIGHT;
+    	const { height } = this.state;
 
-		return (
-			<div className={ _setDisplayClass() }>
+			return (
+				<textarea
+					{ ...input }
+					id={ input.name }
+					type={ type }
+					onFocus={ () => input.onFocus(this.setState({ isActive: true })) }
+					onBlur={ () => input.onBlur(this.setState({ isActive: false })) }
+					placeholder={ placeholder }
+					style={{ height, resize: isOneLine ? "none" : null }}
+					onKeyUp={ this.setFilledTextareaHeight }
+				></textarea>
+			);
+		}
+
+		const getGhostField = (text) => {
+	    return (
+	      <div className={ styles.ghost } ref={ (c) => this.ghost = c } aria-hidden="true">
+	        { text }
+	      </div>
+	    );
+	  }
+
+
+		const renderInput = () => {
+			if (type === 'textarea') {
+				return (
+					<Fragment>
+						{ getGhostField(input.value) }
+						{ getExpandableField() }
+					</Fragment>
+				);
+			}
+			return (
 				<input
 					{ ...input }
 					id={ input.name }
@@ -89,6 +146,12 @@ class BookmarkInput extends Component {
 					placeholder={ placeholder }
 					value={ input.value }
 				/>
+			);
+		}
+
+		return (
+			<div className={ _setDisplayClass() }>
+				{ renderInput() }
 				<label htmlFor={ input.name } className={ _setActiveClass() }>{ label }</label>
 				<div className={ styles.errorMessage }>{ touched && error }</div>
 				{ showInlineBtn() }
