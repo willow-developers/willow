3ced3d66c4f0f4d55d9febb31e45b06c5e9496a0
Tom Wagner <wagne868@umn.edu>
2018-05-23 19:51:50 -0400

    Update prod.js to include process.env keys

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,11 @@
 module.exports = {
 	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
 	EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET,
+	DB_HOST: process.env.DB_HOST,
+	DB_PORT: process.env.DB_PORT,
+	DB_USER: process.env.DB_USER,
+	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_DATABASE: process.env.DB_DATABASE,
 };
