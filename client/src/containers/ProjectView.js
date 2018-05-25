c41fb16286737fe4e7d9de910d212f57d65d6488
Tom Wagner <wagne868@umn.edu>
2018-05-25 14:21:02 -0400

    Bug fixes related to console.log's

--- client/src/containers/ProjectView.js
+++ client/src/containers/ProjectView.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import styles from '../assets/sass/Dashboard.module.scss';
 
