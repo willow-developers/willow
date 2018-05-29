6b99dced1bb5756d9e68d6daaabf9ccd14d3e4c6
Tom Wagner <wagne868@umn.edu>
2018-05-29 01:05:47 -0400

    Clean up back-end code

--- server/index.js
+++ server/index.js
@@ -32,5 +32,5 @@ app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => { console.log(`listening on ${PORT}!!`)});
